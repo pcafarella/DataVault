a5ba9922f2bc281fc2219103f690b0713c046452
Paul Cafarella <paul.cafarella@pacelabs.com>
2024-02-15 19:23:41 -0600

    bridge cleanup

--- DataVault/TSQL/StoredProcedure/bus.usp_bridge_controltower_national.StoredProcedure.sql
+++ DataVault/TSQL/StoredProcedure/bus.usp_bridge_controltower_national.StoredProcedure.sql
@@ -15,11 +15,9 @@ CREATE     PROCEDURE [bus].[usp_bridge_controltower_national] AS
  if object_id('bus.bridge_controltower_national') is not null DROP TABLE bus.bridge_controltower_national
 SELECT   l_test_product.hk_l_test_product AS hk_l_test_product
 		, l_test_sample.hk_l_test_sample AS hk_l_test_sample
-		--, l_workorder_test.hk_l_workorder_test AS hk_l_workorder_test
 		, h_product.hk_h_product AS hk_h_product
 		, h_sample.hk_h_sample AS hk_h_sample
 		, h_test.hk_h_test AS hk_h_test
-		--, h_work_order.hk_h_work_order AS hk_h_work_order
 		, h_product.product_code AS product_code
 		, h_product.product_code_bkcc AS product_code_bkcc
 		, h_sample.sample_no AS sample_no
@@ -27,8 +25,6 @@ SELECT   l_test_product.hk_l_test_product AS hk_l_test_product
 		, h_test.tenant_id AS tenant_id
 		, h_test.test_no AS test_no
 		, h_test.test_no_bkcc AS test_no_bkcc
-		--, h_work_order.work_order_no AS work_order_no
-		--, h_work_order.work_order_no_bkcc AS work_order_no_bkcc
 		, getdate() AS dss_snapshot_date
 	INTO bus.[bridge_controltower_national]
 	FROM  raw.[h_test]
@@ -36,9 +32,6 @@ SELECT   l_test_product.hk_l_test_product AS hk_l_test_product
 	INNER JOIN raw.[h_product] h_product  ON l_test_product.hk_h_product = h_product.hk_h_product
 	INNER JOIN raw.[l_test_sample] l_test_sample ON l_test_sample.hk_h_test = h_test.hk_h_test
 	INNER JOIN raw.[h_sample] h_sample  ON l_test_sample.hk_h_sample = h_sample.hk_h_sample
-	--INNER JOIN raw.[l_workorder_test] l_workorder_test ON l_workorder_test.hk_h_test = h_test.hk_h_test
-	--INNER JOIN raw.[h_work_order] h_work_order ON l_workorder_test.hk_h_work_order = h_work_order.hk_h_work_order
-	--WHERE h_test.dss_load_date <= CAST(getdate() AS DATE)
 
 RETURN 
 
